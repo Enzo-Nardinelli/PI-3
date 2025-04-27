d5d4edfb6339d9874642988868321e66cb3a2e41
Vitória Garcia <vitty.garcia@hotmail.com>
2025-04-27 16:48:18 -0300

    Adição do link de redirecionamento a partir da imagem de editar usuario

--- app/components/headerComponent.js
+++ app/components/headerComponent.js
@@ -4,20 +4,19 @@ import './headerComponent.css';
 import Link from 'next/link';
 import { useNavigate } from 'react-router-dom';
 
-
 const Header = () => {
+  const navigate = useNavigate();
 
-const navigate = useNavigate();
-
-const handleClick = () => {
-      console.log("qualquer coisa")
-      navigate("/carrinho2");
+  const handleClickCarrinho = () => {
+    console.log("Redirecionando para o carrinho");
+    navigate("/carrinho2");
   };
+
   const handleClickUser = () => {
-    console.log("qualquer coisa")
+    console.log("Redirecionando para o login");
     navigate("/login");
-};
-  
+  };
+
   return (
     <header className="header">
       {/* Logo à esquerda */}
@@ -36,10 +35,16 @@ const handleClick = () => {
       {/* Carrinho e usuário à direita */}
       <div className="nav-icons">
         <li className="nav-item">
-            <img src="/imagens/carrinho.png" height="20" width="20" alt="Carrinho" onClick={handleClick}/>
+          <img src="/imagens/carrinho.png" height="20" width="20" alt="Carrinho" onClick={handleClickCarrinho} />
         </li>
         <li className="nav-item">
-            <img src="/imagens/user.png" height="20" width="20" alt="User" onClick={handleClickUser}/>
+          <img src="/imagens/user.png" height="20" width="20" alt="User" onClick={handleClickUser} />
+        </li>
+        {/* Link para a página de editar usuário */}
+        <li className="nav-item">
+          <Link href="/edituser">
+            <img src="/imagens/edit.png" height="20" width="20" alt="Editar Usuário" />
+          </Link>
         </li>
       </div>
     </header>
