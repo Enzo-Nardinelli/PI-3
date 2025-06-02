f588b3c79994c3f2e686e11321f6c5b98040565a
Enzo <enzonms12@gmail.com>
2025-06-01 23:53:05 -0300

    Carrinho temporário muda para usuário logado

--- app/login/page.js
+++ app/login/page.js
@@ -25,9 +25,32 @@ function Login() {
 
       if (data.user) {
         const userWithPassword = { ...data.user, password }; // Adiciona a senha manualmente
+        const temporaryUser = JSON.parse(localStorage.getItem("temporaryUser"));
+        userWithPassword.carrinho = userWithPassword.carrinho.concat(temporaryUser.userCarrinho);
         localStorage.setItem("userLoggedIn", JSON.stringify(userWithPassword));
         console.log("User logged in:", userWithPassword);
 
+        for(let jogo of userWithPassword.carrinho){
+          try {
+            const response = await fetch(`http://localhost:8080/users/${JSON.stringify(userWithPassword.email)}/carrinho/add`, {
+              method: 'PUT',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(jogo), // Send the game ID to add it to the user's cart
+            });
+        
+            if (response.ok) {
+              console.log("Game added to cart in the backend.");
+              navigate("/carrinho2"); // Navigate to the cart page
+            } else {
+              console.error("Failed to add game to cart in the backend.");
+            }
+          } catch (error) {
+            console.error("Error updating the cart:", error);
+          }
+        }
+        
         navigate("/");  // Redireciona para a página inicial após o login
       } else {
         console.error("Invalid user data:", data);
